5a2edcc384a9244a20429503a42cd009334d52f8
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-30 22:54:24 -0500

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import  Home  from './pages/Home';
+import Work from './pages/Work'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
     return(
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/work" element={<Work />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </div>
     );
