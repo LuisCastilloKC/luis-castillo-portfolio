59f4bc952617b0e472f4e301f0f45c9142bc834a
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-02 23:07:01 -0500

    add the welcome text for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Home = () => {
     return(
         <div className="home-container">
-            <h1>Home Page</h1>
+              <div>
+                <h4>Hello</h4>
+                <h4>I'm</h4> 
+                <h1>Luis Castillo</h1>
+                <p>A Full-Stack Web Developer</p> 
+            </div>
         </div>
     );
 };
