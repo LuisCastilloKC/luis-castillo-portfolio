52d285928e67c60d498d8d432bf0645ad6e30adf
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-04 23:56:33 -0500

    add RightNav component and add style in index.html

---
+++ src/components/Navbar/RightNav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RightNav = () => {
+    
+    return(
+        <ul>
+            <li>Home</li>
+            <li>Project</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    )
+}
+
+export default RightNav;
