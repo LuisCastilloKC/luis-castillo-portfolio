eb81f74bf865cd133fc714396c7ccf57a6d73ad7
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-04 23:36:15 -0500

    fix syntax and install styled components

--- src/components/Navbar/MenuHanburger.jsx
+++ src/components/Navbar/MenuHanburger.jsx
@@ -1,11 +1,11 @@
-import React, from 'react';
+import React from 'react';
 
 const MenuHanburger = () => {
     return(
         <div>
             
         </div>
-    )
-}
+    );
+};
 
 export default MenuHanburger;
