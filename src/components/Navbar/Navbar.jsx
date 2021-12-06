4ef97d67e390a54c88f07dcd8622b1cf1e876e05
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-05 22:14:19 -0500

    Styled Right MenuBurger

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import MenuHanburger from './MenuHanburger';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+    
+`
 
 const Navbar = () => {
     return (
-        <div>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/projects">Projects</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-        </div>
+        <Nav> 
+            <div>
+                Nav Bar
+                {/* <NavLink to="/">Home</NavLink>
+                <NavLink to="/projects">Projects</NavLink>
+                <NavLink   NavLink to="/about">About</NavLink>
+                <NavLink to="/contact">Contact</NavLink> */}
+            </div>
+            <MenuHanburger />
+        </Nav>
     );
 }
 export default Navbar;
