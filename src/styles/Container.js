e9905778b25f1fa63138a88a95000f4645e14542
Luis Castillo <mr.luiscastillo@gmail.com>
2022-01-01 18:27:02 -0500

    add ProjectCards CSS

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -60,3 +60,21 @@ import styled from 'styled-components'
         grid-gap: 16px;
         padding: 0 50px;
     `
+    export const ProjectCards = styled.div`
+        background: #fff;
+        display: flex;
+        flex-direction: flex-start;
+        align-items: center;
+        border-radius: 10px;
+        max-height: 340px;
+        padding: 30px;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.2);
+        transform: all 0.2 ease-in-out;
+
+    &:hover {
+        transform: scale(1.02);
+        transition: all 0.2s ease-in-out;
+        cursor: pointer;
+        }
+    `
+
