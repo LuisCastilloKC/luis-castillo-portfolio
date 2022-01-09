a383ede0dc23f71d6bd010441794c0bfd0bd74c9
Luis Castillo <mr.luiscastillo@gmail.com>
2022-01-09 13:43:48 -0500

    delete package-lock.json and update it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import  Home  from './pages/Home';
 import Projects from './pages/Projects'
-// import About from './pages/About'
+import About from './pages/About'
 import Contact from './pages/Contact'
 import Navbar from './components/Navbar/Navbar';
 import GlobalStyles from './styles/Global';
@@ -16,12 +16,12 @@ const App = () => {
             <Navbar />
                 <Routes>
                     <Route path="/#" element={<Home />} />
-                    {/* <Route path="/#about" element={<About />} /> */}
+                    <Route path="/#about" element={<About />} />
                     <Route path="/#projects" element={<Projects />} />
                     <Route path="/#contact" element={<Contact />} />
                 </Routes>
             <Home />
-            {/* <About /> */}
+            <About />
             <Projects />
             <Contact />
         </Router>
