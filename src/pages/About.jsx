0764ddeef0554be361d160a59f11e23a01a4a780
Luis Castillo <mr.luiscastillo@gmail.com>
2022-01-09 14:01:25 -0500

    add trying  git config core.ignorecase false

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AboutContainerStyled, AboutWrapper, AboutContentContainer, AboutH1, AboutP} from '../styles/Container';
+
+const About = () => {
+    return (
+        <> 
+            <AboutContainerStyled id="about">
+                    <AboutH1>About Me</AboutH1>
+                <AboutWrapper>
+                    <AboutContentContainer>
+                        <AboutP>
+                            I was a student at Flatiron School where I spent 10 months learning the fundamentals 
+                            of front-end and back-end web development. I also worked over 10 years in the Retail industry serving
+                            satisfield clients. I have now become a Full-Stack Developer with strong undesting in software programming and engineering.
+                            Skills extend to both client-side and server-side code. A highly collaborative team player, adept at learning
+                            new technology, writing high-quality code, and observing best practices.
+                        </AboutP>
+                    </AboutContentContainer>
+                </AboutWrapper>
+            </AboutContainerStyled>
+        </>
+    );
+}
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FaEnvelope, FaGithub, FaLinkedinIn, FaTwitter} from 'react-icons/fa' 
+import { ContactContainer, 
+        ContactH1, 
+        ContactWrapper, 
+        ContactContent, 
+        ConctactH2, 
+        ContactP, 
+        ContactGitHubIcon, 
+        ContactLinkedInIcon, 
+        ContactTwitterIcon, 
+        ContactEmailIcon,
+        ContactLinkContainer } from '../styles/Container';
+
+const Contact = () => {
+    return (
+        <ContactContainer id="contact">
+            <ContactH1>Say Hello</ContactH1>
+            <ContactWrapper>
+                <ContactContent>
+                    <ConctactH2>Lets Connect!</ConctactH2>
+                    <ContactP>I'm currently open to work. Get in touch 
+                        with one of these method to get to me.</ContactP>
+            <ContactLinkContainer>
+                <ContactGitHubIcon> 
+                    <a href="https://github.com/LuisCastilloKC" rel="noreferrer" target="_blank"><FaGithub /></a>
+                </ContactGitHubIcon>
+                <ContactLinkedInIcon> 
+                    <a href="https://www.linkedin.com/in/luis-castillokc" rel="noreferrer" target="_blank"><FaLinkedinIn /></a>
+                </ContactLinkedInIcon >
+                <ContactTwitterIcon> 
+                    <a href="https://twitter.com/LuisCastilloKC" rel="noreferrer" target="_blank"><FaTwitter /></a>
+                </ContactTwitterIcon>
+                <ContactEmailIcon> 
+                    <a href="mailto:mr.luiscastillo@gmail.com" rel="noreferrer" target="_blank"><FaEnvelope /></a>
+                </ContactEmailIcon>
+            </ContactLinkContainer>
+                </ContactContent>        
+            </ContactWrapper>
+        </ContactContainer>
+    );
+}
+
+export default Contact;
