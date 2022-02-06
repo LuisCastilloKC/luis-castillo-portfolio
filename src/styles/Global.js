03a56026af0fc5690a6b71b3e349532cc8f5a0b9
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-06 05:36:30 -0500

    style Scrollbar for Chrome and Firefox

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -3,19 +3,38 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
+    
+   
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         
+        scrollbar-width: thin;
+        scrollbar-color: rgba(255, 255, 255, 0.9) transparent;
+       
     }
     
     body {
         background-color: #05182c !important;
         font-family: 'Poppins', sans-serif;
         overflow-x: hidden;
-        
+        width: 100vw;
+
+    ::-webkit-scrollbar {
+        width: 6px;
     }
+    ::-webkit-scrollbar-track {
+        background: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: rgba(255, 255, 255, 0.9);
+        border-radius: 10px;
+        border: transparent;
+    }
+
+        
+}
 
 `
 export default GlobalStyles;
